878694e5872100a1538740fba66236b91f306006
Abhishek Malkhedkar <abhishekmalkhedkar3@gmail.com>
2024-11-28 10:43:26 +0530

    Exploring get,post,delete

--- src/app.js
+++ src/app.js
@@ -2,9 +2,22 @@ const express = require("express");
 
 const app = express();
 
-app.use("/", (req,res) => {
-    res.send("Hello from the server..");
-})
+
+app.get("/profile", (req, res) => {
+    res.send("Get request");
+});
+
+app.post("/profile" ,(req, res) => {
+    res.send("Profile Posted Successfully!");
+});
+
+app.put("/profile",(req, res) => {
+    res.send("Profile Updated Successfully!");
+});
+
+app.delete("/profile", (req, res) => {
+    res.send("Profile Deleted Successfully!");
+});
 
 app.listen(3000,()=>{
     console.log("Server running at 3000");
