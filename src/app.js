ca0f5dad503fde4360075eee3e2e7f80dfaf5a22
Abhishek Malkhedkar <abhishekmalkhedkar3@gmail.com>
2024-12-05 16:38:11 +0530

    Created login API

--- src/app.js
+++ src/app.js
@@ -1,24 +1,74 @@
 const express = require("express");
 const connectDb = require("../config/database");
 const User = require("../models/user");
+const { validateSignUpData } = require("../utils/validation");
+const bcrypt = require("bcrypt");
+
+
+
 
 const app = express();
 
 app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-    const user = new User (req.body);
+    //Encrypt the password
+    
+    
+    
+    
     try {
+        //Validation of data
+        validateSignUpData(req);
+        
+        const { firstName, lastName, emailId, password, age } = req.body;
+
+
+        // Encrypt the password
+
+        const passwordHash = await bcrypt.hash(password, 10);
+        console.log(passwordHash);
+        
+        const user = new User({
+            firstName,
+            lastName,
+            emailId,
+            age,
+            password:passwordHash,
+        });
         await user.save();
         res.send("User Added!!");    
     } catch (error) {
-        res.status(400).send("Something went wrong (" + error.message + ")");
+        res.status(400).send(" Error : (" + error.message + ")");
         console.log(error.message);
     }
 
     
 });
 
+
+app.post("/login", async (req, res) => {
+    try{
+        const { emailId, password } = req.body;
+
+        const user = await User.findOne({ emailId : emailId });
+        if(!user){
+            throw new Error("Invalid credentials");
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if(isPasswordValid){
+            res.send("Login Successfull !!");
+        }else{
+            throw new Error("Invalid credentials");
+        }
+
+
+    }catch (error) {
+        res.status(400).send(" Error : (" + error.message + ")");
+        console.log(error.message);
+    }
+})
 
 
 app.get("/user", async (req, res) => {
