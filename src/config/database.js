122f204572827cf77658316d0750fead5feb82ce
Abhishek Malkhedkar <abhishekmalkhedkar3@gmail.com>
2025-03-19 12:21:22 +0530

    Updated Password

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDb = async () => {
     
    await mongoose.connect(
-    "mongodb+srv://abhi:Abhi@cluster0.rveps.mongodb.net/devTinder");
+    "mongodb+srv://abhi:NmDEDLHPSl5GIi5B@cluster0.rveps.mongodb.net/devTinder");
 };
 
 module.exports = connectDb;
